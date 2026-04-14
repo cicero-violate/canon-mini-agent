3938e5e506d6080dfc394f66ba6157d2fc520961
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-14 15:10:50 -0400

    auto

--- send_message.sh
+++ send_message.sh
@@ -1,2 +1,2 @@
-cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --message "any issues?"
+cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --message "make you own fucking plans solo. you fucking lazy fucker"
 cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --read-reply
