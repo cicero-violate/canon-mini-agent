b7cf41fe0c19d56776cc22262d831370ed20f9da
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-14 10:18:23 -0400

    auto

--- tar.sh
+++ tar.sh
@@ -3,8 +3,8 @@ canon-mini-agent/state/rustc \
 canon-mini-agent/tests \
 canon-mini-agent/Cargo.toml \
 canon-mini-agent/src \
-canon-mini-agent/target/debug/deps \
-canon-mini-agent/target/debug/.fingerprint \
-canon-mini-agent/target/debug/build \
+# canon-mini-agent/target/debug/deps \
+# canon-mini-agent/target/debug/.fingerprint \
+# canon-mini-agent/target/debug/build \
 canon-mini-agent/agent_state/llm_full \
 canon/canon-utils/canon-tools-patch
