ca0bde59c3d401e0603c66c1db553f9648f39d1a
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-14 06:12:45 -0400

    auto

--- tar.sh
+++ tar.sh
@@ -1,5 +1,5 @@
 tar -czf canon-mini-agent.tar.gz -C /workspace/ai_sandbox \
-canon-tools-patch = { path = "../canon/canon-utils/canon-tools-patch" } \ 
+
 canon-mini-agent/state/rustc \
 canon-mini-agent/tests \
 canon-mini-agent/Cargo.toml \
