4a0974517bd721ec7337b2a4c1f620441ad74311
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-14 05:50:53 -0400

    auto

--- send_message.sh
+++ send_message.sh
@@ -1,2 +1,2 @@
-cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --message "what is the status report, are we blocked, do you know what to do? do you need my help?"
+cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --message "do an apply_patch now, and force a system restart, we are checking a bug"
 cargo run --bin canon-mini-supervisor -- --workspace /workspace/ai_sandbox/canon-mini-agent --read-reply
