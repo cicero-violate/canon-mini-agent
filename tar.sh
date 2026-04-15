2935d258fcedf06f25f4db8e09c14053ef8128a9
Cicero Bugash <cicero.bugash@gmail.com>
2026-04-15 00:57:13 -0400

    auto

--- tar.sh
+++ tar.sh
@@ -2,7 +2,9 @@ tar -czf canon-mini-agent.tar.gz -C /workspace/ai_sandbox \
 canon-mini-agent/state/rustc \
 canon-mini-agent/agent_state \
 canon-mini-agent/tests \
+canon-mini-agent/INVARIANTS.json \
 canon-mini-agent/Cargo.toml \
+canon-mini-agent/PLAN.json \
 canon-mini-agent/src \
 canon-mini-agent/SPEC.md \
 canon-mini-agent/ISSUES.json \
